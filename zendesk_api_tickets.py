ff74b728a5683dc19cecd659860de26627ca1fd0
John Pham <phammyjohn@hotmail.com>
2021-01-07 08:43:26 +1100

    Update zendesk_api_tickets.py

--- zendesk_api_tickets.py
+++ zendesk_api_tickets.py
@@ -469,15 +469,3 @@ class config():
                 print("not uploading")
         except Exception as e:
             print(e,sys.stderr)
-
-if __name__ == "__main__":
-    config('john.pham@olinqua.com','Aqualite12@',',','`').get_users()
-    config('john.pham@olinqua.com','Aqualite12@',',','`').get_ticket_metrics()
-    config('john.pham@olinqua.com','Aqualite12@',',','`').get_all_tickets()
-    config('john.pham@olinqua.com','Aqualite12@',',','`').get_orgs()
-    config('john.pham@olinqua.com','Aqualite12@',',','`').get_groups()
-    config('john.pham@olinqua.com','Aqualite12@',',','`').get_tags()
-    #config('john.pham@olinqua.com','Aqualite12@',',','`').get_incremental_ticket()
-    config('john.pham@olinqua.com','Aqualite12@',',','`').get_incremental_ticket_events()
-    config('john.pham@olinqua.com','Aqualite12@',',','`').get_metrics_events()
-
